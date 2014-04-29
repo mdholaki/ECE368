e31eedda25d0872037f2f167f153e15e4e223bf6
mdholaki <mdholaki@purdue.edu>
2014-04-28 21:14:54 -0400

    working on vertices

--- PA05/river.h
+++ PA05/river.h
@@ -5,9 +5,21 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+typedef struct _vertex{
+  int col;
+  int start_row;
+  int end_row;
+}Vertex;
+
+/*typedef struct _edge{
+  int vertex_idx;
+  int weight;
+  edge * next;
+}Edge; */
+
 
 //Function Declarations
-char * LoadFile(char * filename, int * size);
-
-
+char * LoadFile(char * filename, int * size, int * N);
+Vertex * BuildGraph(char * array, int size, int N);
+int num_vert(char * array, int size, int N);
 #endif
