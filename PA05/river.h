df6d911a67cb92c77a95cbeb287d4a011833f5a6
mdholaki <mdholaki@purdue.edu>
2014-04-29 12:16:07 -0400

    Fix Bellman/Widths

--- PA05/river.h
+++ PA05/river.h
@@ -25,6 +25,7 @@ Vertex * BuildGraph(char * array, int size, int N);
 int FindWeight(Vertex * vert_list, int source, int dest);
 int num_vert(char * array, int size, int N);
 void adj_list_build(Vertex * vert_list, int size, int N, int verts);
+void Bellman_Ford(Vertex * vert_list, int verts, int N);
 Edge * newEdge(int index, int weight);
 
 
